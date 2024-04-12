6de16eb5093cc392ec3a53e602e04b731a1bed1f
MDrenova <mateodrenova@gmail.com>
2024-04-12 11:33:54 +0200

    Update: pycodestyle and html

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -23,5 +23,6 @@ def states_list():
     states = storage.all("State").values()
     return render_template('7-states_list.html', states=states)
 
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
