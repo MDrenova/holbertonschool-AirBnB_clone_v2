8154d232d70978e5a722d354f0ccd83374da1299
MDrenova <mateodrenova@gmail.com>
2024-04-12 12:36:25 +0200

    Create: web app to show all states and their cities, and created the template in html

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Web application"""
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join
+                                   (os.path.dirname(__file__), '..')))
+from flask import Flask, render_template  # noqa: E402
+from models import storage  # noqa: E402
+from models.state import State  # noqa: E402
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_all(error):
+    """
+    func to remove sqlalchemy session
+    """
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """
+    States display
+    """
+    states = storage.all("State").values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def states_list_cities():
+    """
+    States display
+    """
+    states = storage.all("State").values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
