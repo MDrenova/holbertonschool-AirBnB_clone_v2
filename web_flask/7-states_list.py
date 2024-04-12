99e1157c3176fe39860b1818eddb0ea42cdb5759
MDrenova <mateodrenova@gmail.com>
2024-04-12 11:36:59 +0200

    Update: code and html

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,27 +2,19 @@
 """Web application"""
 from flask import Flask, render_template
 from models import storage
-from models.state import State
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-
-@app.teardown_appcontext
-def close_all(error):
-    """
-    func to remove sqlalchemy session
-    """
-    storage.close()
-
-
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list')
 def states_list():
-    """
-    States display
-    """
     states = storage.all("State").values()
+    states = sorted(states, key=lambda state: state.name)
     return render_template('7-states_list.html', states=states)
 
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
